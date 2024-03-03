57dc3e1bd3f79181d1c433113c4c085fc80525c4
Droopytex <a.velasquezvega@gmail.com>
2024-03-03 02:04:20 -0300

    entrega de desafío java 01

---
+++ assets/js/index.js
@@ -0,0 +1,14 @@
+const precio = 400000;
+const cantidad = 0;
+const precioInicial = 0;
+
+const valorTotal = precio * cantidad;
+
+precioSpan = document.querySelector(".precio-inicial");
+precioSpan.innerHTML = precio;
+
+precioInicialSpan = document.querySelector(".cantidad");
+
+cantidadSpan = document.querySelector(".cantidad");
+
+valorTotalSpan = document.querySelector(".valor-total");
